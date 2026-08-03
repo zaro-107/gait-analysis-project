c5035a8b139c1aa272d84df6e3fc412a6037f474
Pradhuman Singh Shekhawat <spradhuman107@gmail.com>
2026-08-03 19:38:48 +0530

    Update App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import GaitDashboard from "./GaitDashboard";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
+const API_BASE = "/api";
 
 function pretty(obj) {
   try {
